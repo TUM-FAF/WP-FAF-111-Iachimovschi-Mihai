0fc1d2fb51a5eebdc8fa270157535c6d219db472
Mihai Iachimovschi <mihai.iachimovschi@gmail.com>
2013-02-21 20:13:12 +0200

    Initial commit

---
+++ Lab#1/main.c
@@ -0,0 +1,132 @@
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+
+#define IDC_ADD_BUTTON 100
+#define IDC_COUNT_BUTTON 101
+
+LPSTR szClassName = "MyCustomClass";
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE hInst;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLine, int iCmdShow)
+{
+    WNDCLASSEX wnd;
+    MSG msg;
+    HWND hwnd;
+    hInst = hInstance;
+
+    /* The Window Structure */
+    wnd.hInstance = hInst;
+    wnd.lpszClassName = szClassName;
+    wnd.lpfnWndProc = WindowProcedure;
+    wnd.style = CS_HREDRAW | CS_VREDRAW;
+    wnd.cbSize = sizeof(WNDCLASSEX);
+
+    /* Use default icon and mouse-pointer */
+    wnd.hIcon = LoadIcon(NULL, IDI_APPLICATION);                                // Default icon
+    wnd.hIconSm = LoadIcon(NULL, IDI_APPLICATION);                              // Default icon
+    wnd.hCursor = LoadCursor(NULL, IDC_ARROW);                                  // Default arrow mouse cursor
+    wnd.lpszMenuName = NULL;                                                    // No menu
+    wnd.cbClsExtra = 0;                                                         // No extra bytes after the window class
+    wnd.cbWndExtra = 0;                                                         //  structure or the window instance
+
+    wnd.hbrBackground = (HBRUSH)(COLOR_BACKGROUND);
+
+    if(!RegisterClassEx(&wnd))                                                  // Register the WNDCLASSEX
+    {
+        MessageBox(NULL, "This Program Requires Windows NT", "Error", MB_OK);
+        return 0;
+    }
+
+    hwnd = CreateWindow(
+        szClassName,
+        "Lab#1",                                                                // Window title
+        WS_OVERLAPPEDWINDOW,                                                    // Basic window style
+        CW_USEDEFAULT, CW_USEDEFAULT,                                           // Set starting point to default value
+        400, 300,                                                               // Set all the dimensions to default value
+        NULL,                                                                   //no parent window
+        NULL,                                                                   //no menu
+        hInst,
+        NULL);                                                                  //no parameters to pass
+
+    ShowWindow(hwnd, iCmdShow);                                                 //display the window on the screen
+    UpdateWindow(hwnd);                                                         //make sure the window is updated correctly
+
+    while(GetMessage(&msg, NULL, 0, 0))                                         //message loop
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return msg.wParam;
+}
+
+LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    static HWND hwndAddButton, hwndCountButton, hwndText;
+    RECT rect ;
+    PAINTSTRUCT ps ;
+    HDC hdc;
+    LPSTR title = _T("A simple and awesome window.");
+
+    switch(msg)
+    {
+        case WM_CREATE:
+            hwndAddButton = CreateWindow(
+                TEXT("button"),                                                 // The class name required is button
+                TEXT("Push Button"),                                            // the caption of the button
+                WS_CHILD |WS_VISIBLE | BS_PUSHBUTTON,                           // the styles
+                5, 200,                                                         // the left and top co-ordinates
+                100, 40,                                                        // width and height
+                hwnd,                                                           // parent window handle
+                (HMENU)IDC_ADD_BUTTON,                                          // the ID of your button
+                hInst,                                                          // the instance of your application
+                NULL);
+
+            hwndCountButton = CreateWindow(
+                TEXT("button"),                                                 // The class name required is button
+                TEXT("Push Button2"),                                           // the caption of the button
+                WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,                          // the styles
+                110, 200,                                                       // the left and top co-ordinates
+                100, 40,                                                        // width and height
+                hwnd,                                                           // parent window handle
+                (HMENU)IDC_COUNT_BUTTON,                                        // the ID of your button
+                hInst,                                                          // the instance of your application
+                NULL);                                                          // extra bits you dont really need
+
+            hwndText = CreateWindow(
+                TEXT("edit"),                                                   // The class name required is edit
+                TEXT(""),                                                       // Default text.
+                WS_VISIBLE | WS_CHILD | WS_BORDER | ES_MULTILINE | ES_AUTOHSCROLL,     // the styles
+                5, 20,                                                          // the left and top co-ordinates
+                382, 140,                                                       // width and height
+                hwnd,                                                           // parent window handle
+                (HMENU)11,                                                      // the ID of your editbox
+                hInst,                                                          // the instance of your application
+                NULL);                                                          // extra bits you dont really need
+            break;
+
+        case WM_PAINT:
+            hdc = BeginPaint(hwnd, &ps);
+            GetClientRect(hwnd, &rect);                                         // Getting coordinates of window client area
+            SetBkMode(hdc, TRANSPARENT);                                        // Set the background to be transparent
+            SetTextColor(hdc, RGB(152, 0, 16));                                 // Setting the text color
+            DrawText(hdc, title, -1, &rect, DT_CENTER | DT_TOP);                // Drawind the text on top aligning it to center
+            SetTextColor(hdc, RGB(0, 0, 0));                                    // Resetting the color
+            EndPaint(hwnd, &ps);
+            break;
+        case WM_CLOSE:
+            if(MessageBox(hwnd, "Are you sure that you want to close this awesome window?", "Lab#1", MB_OKCANCEL) == IDOK)
+                DestroyWindow(hwnd);
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
+
