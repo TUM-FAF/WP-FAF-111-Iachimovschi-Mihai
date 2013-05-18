8954fe3933dba338bf60b2282b65509581f539ac
Mihai Iachimovschi <mihai.iachimovschi@gmail.com>
2013-05-18 12:25:34 +0300

    Adding slide from bottom bar with link to the site.

--- Lab#6-#7/FuckingGreatAdvice/pages/home/home.js
+++ Lab#6-#7/FuckingGreatAdvice/pages/home/home.js
@@ -5,6 +5,12 @@
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
+            element.querySelector("#visitSiteButton").addEventListener("click", function () {
+                var siteUri = new Windows.Foundation.Uri('http://fucking-great-advice.ru/');
+
+                Windows.System.Launcher.launchUriAsync(siteUri);
+            });
+
             var adviceListView = element.querySelector("#adviceListView").winControl;
             adviceListView.oniteminvoked = this._itemInvoked.bind(this);
         },
