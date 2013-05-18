599cb940cfb7bb7aff830b55c912a3ea42b488cf
Mihai Iachimovschi <mihai.iachimovschi@gmail.com>
2013-05-18 09:42:37 +0300

    Fucking great advice APP grabber app ;]

---
+++ Lab#6-#7/FuckingGreatAdvice/js/data.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="//Microsoft.WinJS.1.0/js/base.js" />
+(function () {
+    "use strict";
+
+    var advices = new WinJS.Binding.List();
+
+    var apiUrl = "http://fucking-great-advice.ru/api/latest/40";
+
+    WinJS.xhr({ url: apiUrl }).then(function (xhr) {
+
+        var items = JSON.parse(xhr.responseText);
+
+        items.forEach(function (item) {
+            item.text = item.text.replace('&nbsp;', ' ');
+            item.teaser = item.text.substr(0, 10);
+            item.teaser += " ...";
+            item.textId = "Advice #" + item.id;
+            advices.push(item);
+        });
+    });
+
+    WinJS.Namespace.define("Data", {
+        allAdvices: advices
+    });
+
+})();
---
+++ Lab#6-#7/FuckingGreatAdvice/pages/home/home.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            var adviceListView = element.querySelector("#adviceListView").winControl;
+            adviceListView.oniteminvoked = this._itemInvoked.bind(this);
+        },
+
+        _itemInvoked: function (args) {
+            var selectedItem = Data.allAdvices.getAt(args.detail.itemIndex);
+
+            WinJS.Navigation.navigate("/pages/detail/detail.html",
+                { item: selectedItem });
+        }
+    });
+})();
